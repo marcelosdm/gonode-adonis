1af212df0f9e0b2c4d80ebe47dc2ac5a138011e5
Wemerson Guimarães <wemersonrv@gmail.com>
2018-11-12 14:18:29 -0200

    Comentar ponto do erro

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -8,6 +8,7 @@ class UserController {
     const data = await request.only(['username', 'email', 'password'])
     const addresses = request.input('addresses')
 
+    // Quando algo falha, a request roda até dar timeout
     const trx = await Database.beginTransaction()
     const user = await User.create(data, trx)
     await user.addresses().createMany(addresses, trx)
