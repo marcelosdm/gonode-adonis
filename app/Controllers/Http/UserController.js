361bedb10df9e0b02b2c5fde898288ca156fb157
Wemerson Guimarães <wemersonrv@gmail.com>
2018-11-12 15:54:41 -0200

    Add Rollback

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -4,17 +4,21 @@ const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
-  async store ({ request }) {
+  async store ({ request, response }) {
     const data = await request.only(['username', 'email', 'password'])
     const addresses = request.input('addresses')
 
-    // Quando algo falha, a request roda até dar timeout
+    // Ajustado para fazer rollback na transação e disparar erro em seguida
     const trx = await Database.beginTransaction()
-    const user = await User.create(data, trx)
-    await user.addresses().createMany(addresses, trx)
-    await trx.commit()
-
-    return user
+    try {
+      const user = await User.create(data, trx)
+      await user.addresses().createMany(addresses, trx)
+      await trx.commit()
+      return user
+    } catch (err) {
+      await trx.rollback()
+      throw err
+    }
   }
 }
 
